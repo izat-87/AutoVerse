f81f6eb19a110ce55fb74f46146c85e60e457345
|IZATCODE| <izat87597@gmail.com>
2026-08-06 13:27:29 +0600

    Prefer real images: use car.image/guide.image/item.image before generateCarImage

--- js/pages/home.js
+++ js/pages/home.js
@@ -16,11 +16,11 @@ window.AutoPages = window.AutoPages || {};
 
     // Featured car for hero
     const heroCar = cars.find(function (c) { return c.id === 'bugatti-chiron'; }) || cars[0];
-    const heroImg = window.AutoComponents.generateCarImage ? window.AutoComponents.generateCarImage(heroCar, { width: 1600, height: 900 }) : heroCar.image;
+    const heroImg = heroCar.image || (window.AutoComponents.generateCarImage ? window.AutoComponents.generateCarImage(heroCar, { width: 1600, height: 900 }) : '');
 
     // Featured showcase car
     const showcaseCar = cars.find(function (c) { return c.id === 'lamborghini-aventador'; }) || cars[1];
-    const showcaseImg = window.AutoComponents.generateCarImage ? window.AutoComponents.generateCarImage(showcaseCar, { width: 1200, height: 600 }) : showcaseCar.image;
+    const showcaseImg = showcaseCar.image || (window.AutoComponents.generateCarImage ? window.AutoComponents.generateCarImage(showcaseCar, { width: 1200, height: 600 }) : '');
 
     // Popular cars (top by popularity)
     const popular = [...cars].sort(function (a, b) { return b.popularity - a.popularity; }).slice(0, 4);
@@ -45,7 +45,7 @@ window.AutoPages = window.AutoPages || {};
         // Hero
         '<section class="home-hero">' +
           '<div class="home-hero__bg">' +
-            '<img src="' + heroImg + '" alt="' + heroCar.name + '" class="home-hero__bg-img" />' +
+            '<img src="' + heroImg + '" alt="' + (heroCar ? heroCar.name : '') + '" class="home-hero__bg-img" />' +
           '</div>' +
           '<div class="container">' +
             '<div class="home-hero__content">' +
